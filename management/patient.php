0f5f4b255047bf694371b135ff384f3fe8f58cd3
aziz_boujaada <azizboujaada87@gmail.com>
2025-12-25 12:08:47 +0100

    UCCC-10 UCCC-11 #done feat:add methode of delete patient and find by id

--- management/patient.php
+++ management/patient.php
@@ -1,5 +1,5 @@
 <?php
-include "./management/uesr.php";
+include "./management/user.php";
 require_once "./config/confin_db.php";
 //extend class patients from user 
 class Patient extends user
@@ -8,8 +8,8 @@ class Patient extends user
     public $age;
     public $gender;
     public $adress;
-    
-    private  $connection ;
+
+    private  $connection;
     function __construct(
         $first_name = null,
         $last_name = null,
@@ -21,36 +21,75 @@ class Patient extends user
     ) {
         $db = new Database();
         $this->connection = $db->Connect();
-        
-        parent::__construct($first_name , $last_name , $phone_number , $email);
+
+        parent::__construct($first_name, $last_name, $phone_number, $email);
         $this->age = $age;
         $this->gender = $gender;
         $this->adress = $adress;
-
     }
 
-    public function addPatient($data){
-      
+    public function addPatient($data)
+    {
 
-    
 
-          $query = "INSERT INTO patients (first_name , last_name , email , age ,phone_number , gender ,adress ) 
+
+
+        $query = "INSERT INTO patients (first_name , last_name , email , age ,phone_number , gender ,adress ) 
                  VALUES(:first_name , :last_name , :email , :age ,:phone_number, :gender ,:adress)";
-       $stmt = $this->connection->prepare($query);
-       $stmt->bindParam(":first_name" , $data['first_name']);
-       $stmt->bindParam(":last_name" , $data['last_name']);
-       $stmt->bindParam(":email" , $data['email']);
-       $stmt->bindParam(":age" , $data['age']);
-       $stmt->bindParam(":phone_number" , $data['phone_number']);
-       $stmt->bindParam(":gender" , $data['gender']);
-       $stmt->bindParam(":adress" , $data['adress']);
-       
-       if($stmt->execute()){
-           echo "Patients added succussfuly" . "\n";
-        }else{
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(":first_name", $data['first_name']);
+        $stmt->bindParam(":last_name", $data['last_name']);
+        $stmt->bindParam(":email", $data['email']);
+        $stmt->bindParam(":age", $data['age']);
+        $stmt->bindParam(":phone_number", $data['phone_number']);
+        $stmt->bindParam(":gender", $data['gender']);
+        $stmt->bindParam(":adress", $data['adress']);
+
+        if ($stmt->execute()) {
+            echo "Patients added succussfuly" . "\n";
+        } else {
             echo "added failed" . "\n";
         }
-    
+    }
+    public function findByID($id)
+    {
+        $query = "SELECT * FROM patients  WHERE patient_id = :id";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(":id", $id);
+        if ($stmt->execute()) {
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                return $row;
+            }
+        }
+    }
 
+    public function editPatient($data)
+    {
+
+        $query = "UPDATE patients SET  
+          first_name = :first_name  , 
+          last_name = :last_name,
+          email = :email ,
+          age = :age ,
+          phone_number = :phone_number , 
+          gender = :gender ,
+          adress = :gender            
+          WHERE patient_id = :id";
+
+        $id = $data['id'];
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->bindParam(":first_name", $data['first_name']);
+        $stmt->bindParam(":last_name", $data['last_name']);
+        $stmt->bindParam(":email", $data['email']);
+        $stmt->bindParam(":age", $data['age']);
+        $stmt->bindParam(":phone_number", $data['phone_number']);
+        $stmt->bindParam(":gender", $data['gender']);
+        $stmt->bindParam(":adress", $data['adress']);
+        if ($stmt->execute()) {
+            echo "Patients UPDATED succussfuly" . "\n";
+        } else {
+            echo "UPDATED failed" . "\n";
+        }
     }
 }
--- navigation/patientMenu.php
+++ navigation/patientMenu.php
@@ -47,14 +47,63 @@ do {
                 "phone_number" => $phone_number,
                 "adress" => $adress
             ];
-              
-             $patient = new Patient();
-             $patient->addPatient($data);
+
+            $patient = new Patient();
+            $patient->addPatient($data);
+
 
             break;
 
-            //edit a patient 
-            case 2 : 
-                
+        //edit a patient 
+        case 2:
+
+
+            //     $Patient = new Patient();
+            //    $Patient->findByID($id);
+
+
+
+            printf("Enter ID to Edit :");
+            $id = (int)trim(fgets(STDIN));
+
+            printf("Enter first name :");
+            $first_name = trim(fgets(STDIN));
+
+            printf("Enter your last name");
+            $last_name = trim(fgets(STDIN));
+
+            printf("Enter your age");
+            $age = trim(fgets(STDIN));
+
+            printf("Enter your email");
+            $email = trim(fgets(STDIN));
+
+            printf("Enter your phone number");
+            $phone_number = trim(fgets(STDIN));
+
+            printf("Enter your gender (male or female)");
+            $gender = trim(fgets(STDIN));
+
+            printf("Enter your adress");
+            $adress = trim(fgets(STDIN));
+
+            $data = [
+                "id" => $id,
+                "first_name" => $first_name,
+                "last_name" => $last_name,
+                "age" => (int)$age,
+                "email" => $email,
+                "gender" => $gender,
+                "phone_number" => $phone_number,
+                "adress" => $adress
+            ];
+
+            $patient = new Patient();
+            $patient->editPatient($data);
+            break;
+
+
+        case 3:
+          /// delete 
     }
-} while ($patientChoice !== 5);
+} while ((int)$patientChoice !== 5);
